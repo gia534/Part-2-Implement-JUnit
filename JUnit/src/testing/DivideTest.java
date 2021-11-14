1e8d1920679efa0132c415a3d0e684c2cb25a3b7
Gaelle Gilles <67564107+gia534@users.noreply.github.com>
2021-11-14 08:42:21 -0500

    Small changes to code

--- JUnit/src/testing/DivideTest.java
+++ JUnit/src/testing/DivideTest.java
@@ -9,13 +9,12 @@ class DivideTest {
     @Test
     void divide() {
         JunitTesting test = new JunitTesting();
-        int output = (int) test.divide(-6, 3);
-        assertEquals(-2, output);
+        int correctOutput = (int) test.divide(-6, 3);
+        assertEquals(-2, correctOutput);
 
-        int positiveNum = (int) test.divide(10, 5);
-        assertEquals(2, positiveNum);
+//        int wrongOutput = (int) test.divide(6, 3);
+//        assertEquals(-2, wrongOutput);
 
-        int zero = (int) test.divide(9, 0);
-        assertEquals(0, zero);
+
     }
 }
